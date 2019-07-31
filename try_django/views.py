c5789656ce9140ce70881e22d452105398503e5e
marvin evins <maevins@cps.edu>
2019-07-31 18:21:03 -0500

    working on templating

--- try_django/views.py
+++ try_django/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
-    return HttpResponse("<h1> helo world</h1>")
+    my_title="hello there"
+    context = {"title": my_title, "my_list": [1,2,3,4,5]}
+    return render(request, "home.html",context )
 
 def about_page(request):
-    return HttpResponse("<h1> about</h1>")
+    return render(request, "about.html",{"title": "About"})
 
 def contact_page(request):
-    return HttpResponse("<h1> contact</h1>")
+    return render(request, "contact.html",{"title": "contact"})
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = "hello_world.html"
+    template_obj = get_template(template_name)
+    return HttpResponse(template_obj.render(context))
