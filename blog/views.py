cae33b5724fb955d933edb44526075b68a0b6cc3
marvin evins <maevins@cps.edu>
2019-08-06 09:39:06 -0500

    working on queryset

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
@@ -8,11 +8,17 @@ from .models import BlogPost
 
 
 
-def blog_post_detail_page(request, id):
-    try:
-        obj = BlogPost.objects.get(id=id)
-    except:
-        raise Http404
+def blog_post_detail_page(request, slug):
+    queryset = BlogPost.objects.filter(slug = slug)
+    if
+
+    #obj = get_object_or_404(BlogPost, slug=slug)
+    # try:
+    #     obj = BlogPost.objects.get(id=id)
+    # except BlogPost.DoesNotExist:
+    #     raise Http404
+    # except ValueError:
+    #     raise Http404
     template_name = 'blog_post_detail.html'
     context = {"object":obj}
 
--- try_django/urls.py
+++ try_django/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from ..blog.views import(
+from blog.views import(
    blog_post_detail_page
 )
 
@@ -23,7 +23,7 @@ from .views import (home_page, about_page, contact_page,example_page)
 
 urlpatterns = [
     path('', home_page),
-    path('blog/<int: id>/', blog_post_detail_page),
+    path('blog/<str: slug>/', blog_post_detail_page),
     path('about/', about_page),
     path('contact/', contact_page),
     path('example/', example_page),
